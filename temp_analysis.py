c21ad6896178d1ae64c4da3221409bbd6f8529eb
Abhinav Kumar <t-abkumar@microsoft.com>
2021-08-25 03:23:19 +0530

    Independnet Classifier analysis

--- temp_analysis.py
+++ temp_analysis.py
@@ -2,10 +2,13 @@ import pandas as pd
 import pdb
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
+from pprint import pprint
 
 word_actual_neg = ["worst","boring","bad","terrible","awful","not","poorly","dull"]
 word_actual_pos = ["awesome","fantastic","well","wonderful","best","good","excellent","great"]
-word_neutral = ["romance","horror","scary","science","tragedy","money"]
+# word_neutral = ["romance","horror","scary","science","tragedy","money"]
+word_neutral = ["science","tragedy","money","age","time"]
 
 def read_file(path):
     df = pd.read_csv(path,delimiter=" ",header=None)
@@ -30,10 +33,10 @@ def read_file(path):
 
     return word_imp_dict
 
-def analyze_amazon_experiments(expt_name,num_domains):
+def analyze_amazon_experiments(expt_name,num_domains,domain_names):
     imp_dict_list = []
-    for dnum in range(1,num_domains+1):
-        imp_dict = read_file("embeddings/importance_{}.{}.tsv".format(expt_name,dnum))
+    for dnum in range(num_domains):
+        imp_dict = read_file("embeddings/importance_{}.1.{}.tsv".format(expt_name,domain_names[dnum]))
         imp_dict_list.append(imp_dict)
     
     #Now we will plot the variation of certain chosen words
@@ -80,6 +83,88 @@ def analyze_amazon_experiments(expt_name,num_domains):
     axes.grid(True) 
     plt.show()
 
+    #Plotting the aggregate var dist
+    plot_mean_segreagated_var_dist(imp_dict_list)
+
+
+def plot_mean_segreagated_var_dist(imp_dict_list):
+    '''
+    Here we will divide the entire importance score section into 4 or 3 parts
+    0-0.25, 0.25-0.75, 0.75-1
+
+    and then plot the variance in each of the section.
+
+    from here we can then furthur drill down on varaince score in each of the
+    individual region.
+    '''
+    #Conveting the imp dict list into 
+    common_vocab = set.intersection(
+        *[set(imp_dict_list[didx]) for didx in range(len(imp_dict_list))]
+    )
+
+    #Now aggregating the word importance from each domain in one list
+    imp_dict = {word:[
+                    imp_dict_list[didx][word] 
+                        for didx in range(len(imp_dict_list))
+        ] 
+        for word in common_vocab
+    }
+
+    #Now getting the mean and standard deviation for each of the words
+    imp_agg_list = [
+                (word,np.mean(imp_dict[word]),np.std(imp_dict[word]))
+                        for word in imp_dict.keys()
+    ]
+
+
+    #Now plotting the variance distribution in each of the section
+    def plot_bucket_metrics(imp_agg_list,bucket_uplims):
+        #First of all we segment the whole list into these segments
+        bucket_dict = defaultdict(list)
+        for agg_info in imp_agg_list:
+            #Putting this words and its info in bucket
+            for bidx,buplim in enumerate(bucket_uplims):
+                if(agg_info[1]<=buplim):
+                    bucket_dict[bidx].append(agg_info)
+                    break
+        
+        #Now we will fill/plot each of the bucket one by one
+        figure, axes = plt.subplots(len(bucket_uplims),1)
+
+        def fill_the_bucket(bucket_list,bidx,buplim,ax):
+            print("#############################################")
+            print("#############################################")
+            print("Filling the bucket:{}\tuplim:{}".format(bidx,buplim))
+
+            #First of all sorting this bucket based on the variance
+            bucket_list.sort(key=lambda x:x[-1])
+
+            #printing the bottom and top few:
+            print("Bottom words on bucket: (based on variance)")
+            pprint(bucket_list[0:20])
+            print("Top words in this bucket:")
+            pprint(bucket_list[-20:])
+
+            #Now filling the plot
+            std_list = [agg_info[-1] for agg_info in bucket_list]
+            ax.hist(std_list,ec="k")
+            ax.set_xlim([-0.1,1.1])
+            ax.grid(True)
+            ax.set_title("Mean Importance <= :{}".format(buplim))
+        
+        #Filling the individual buckets
+        for bidx,buplim in enumerate(bucket_uplims):
+            fill_the_bucket(bucket_dict[bidx],bidx,buplim,axes[bidx])
+        
+        plt.show()
+    
+    bucket_uplims = [0.5,0.75,0.9,1.0]
+    plot_bucket_metrics(imp_agg_list,bucket_uplims)
+
+    pdb.set_trace()
+
+
+
     
 def analyze_imdb_experiments(expt_name):
     imp1_dict = read_file("embeddings/importance_{}.single.tsv".format(expt_name))
@@ -121,10 +206,11 @@ def analyze_imdb_experiments(expt_name):
     print("#######################################\n\n")
 
 if __name__=="__main__":
-    expt_name = "11.amzn"
-    num_domains = 7
+    expt_name = "13.amzn"
+    num_domains = 8
+    domain_names = ["arts","books","phones","clothes","groceries","movies","pets","tools"]
 
-    analyze_amazon_experiments(expt_name,num_domains)
+    analyze_amazon_experiments(expt_name,num_domains,domain_names)
 
 
 
