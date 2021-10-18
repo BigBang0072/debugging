ee7c9d576ffe864a2223c0690333fb4b32f03adb
It's Me! <t-abkumar@microsoft.com>
2021-10-18 10:53:59 +0000

    notebook and expt file commit

--- exp.sh
+++ exp.sh
@@ -307,7 +307,19 @@ mkdir nlp_logs
 
 # python transformer_debugger.py -expt_num "ct9.temp" -num_topics 10 -num_epochs 30 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 
 
-python transformer_debugger.py -expt_num "ct10.0" -num_topics 10 -num_epochs 15 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 -stage 2 -debug_cidx 1 -debug_tidx 6
+# python transformer_debugger.py -expt_num "ct10.0" -num_topics 10 -num_epochs 15 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 -stage 2 -debug_cidx 1 -debug_tidx 6
 
-python transformer_debugger.py -expt_num "ct10.0.adv" -num_topics 10 -num_epochs 15 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 -stage 2 -debug_cidx 1 -debug_tidx 6 --reverse_grad
+# python transformer_debugger.py -expt_num "ct10.0.adv" -num_topics 10 -num_epochs 15 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 -stage 2 -debug_cidx 1 -debug_tidx 6 --reverse_grad
+
+# python transformer_debugger.py -expt_num "ct10.0.adv" -num_topics 10 -num_epochs 15 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.90 -stage 2 -debug_cidx 1 -debug_tidx 0 --reverse_grad
+
+
+#Making the current very toyish dataset more clear
+python transformer_debugger.py -expt_num "ct11.0" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.85 -stage 1
+python transformer_debugger.py -expt_num "ct11.1" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.88 -stage 1
+python transformer_debugger.py -expt_num "ct11.2" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.91 -stage 1
+python transformer_debugger.py -expt_num "ct11.3" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.94 -stage 1
+python transformer_debugger.py -expt_num "ct11.4" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 0.97 -stage 1
+python transformer_debugger.py -expt_num "ct11.5" -num_topics 10 -num_epochs 70 -transformer "bert-base-uncased" -l1_lambda 0.0 -temb_dim 1 -path "dataset/nlp_toy/data/" -causal_ratio 0.85 -spurious_ratio 1.0 -stage 1
+
 
