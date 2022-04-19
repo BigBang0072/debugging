21f891d4854a0a5ea4a775658a66eca4b3207d53
kabirahuja2431 <f2015791p@alumni.bits-pilani.ac.in>
2022-04-19 19:16:42 +0000

    Adding the analysis file

--- exp.sh
+++ exp.sh
@@ -1622,40 +1622,40 @@ mkdir nlp_logs
 
 
 #Training the bert model
-for loss_type in "x_entropy"
-do
-    for hretrain in "no_warm_encoder"
-    do
-        for r in 0
-        do
-            for dropout_rate in 0.0
-            do
-                for l2_lambd in 0.0
-                do
-                    for e in 5
-                    do
-                        for d in "non_causal"
-                        do
-                            for h in 0
-                            do
-                                for s in 10000
-                                do
-                                    for n in 0.0
-                                    do
-                                        for p in 0.5 0.6 0.7 0.8 0.9 0.99
-                                        do
-                                            python transformer_debugger.py -expt_num "pt.rel.lt($loss_type).dropout_rate($dropout_rate).l2($l2_lambd).hretrain($hretrain).d($d).n($n).h($h).s($s).e($e).p($p).r($r)" -num_sample $s -num_topics 1 -num_epochs $e -path "dataset/multinli_1.0/" -emb_path "glove-wiki-gigaword-100" -noise_ratio $n -num_hidden_layer $h -stage 2 -main_model_mode $d --normalize_emb -lr 5e-5 -head_retrain_mode $hretrain -l2_lambd $l2_lambd -loss_type $loss_type -dropout_rate $dropout_rate -dtype "nlp_bert" --bert_as_encoder --train_bert -neg_topic_corr $p
-                                        done
-                                    done
-                                done
-                            done       
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+# for loss_type in "x_entropy"
+# do
+#     for hretrain in "no_warm_encoder"
+#     do
+#         for r in 0
+#         do
+#             for dropout_rate in 0.0
+#             do
+#                 for l2_lambd in 0.0
+#                 do
+#                     for e in 3
+#                     do
+#                         for d in "non_causal"
+#                         do
+#                             for h in 0
+#                             do
+#                                 for s in 20000
+#                                 do
+#                                     for n in 0.0
+#                                     do
+#                                         for p in 0.5 0.6 0.7 0.8 0.9 0.99
+#                                         do
+#                                             python transformer_debugger.py -expt_num "pt.rel.lt($loss_type).dropout_rate($dropout_rate).l2($l2_lambd).hretrain($hretrain).d($d).n($n).h($h).s($s).e($e).p($p).r($r)" -num_sample $s -num_topics 1 -num_epochs $e -path "dataset/multinli_1.0/" -emb_path "glove-wiki-gigaword-100" -noise_ratio $n -num_hidden_layer $h -stage 2 -main_model_mode $d --normalize_emb -lr 5e-5 -head_retrain_mode $hretrain -l2_lambd $l2_lambd -loss_type $loss_type -dropout_rate $dropout_rate -dtype "nlp_bert" --bert_as_encoder --train_bert -neg_topic_corr $p
+#                                         done
+#                                     done
+#                                 done
+#                             done       
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
 
 
@@ -2599,6 +2599,59 @@ done
 #         done
 #     done
 # done
+
+
+
+#Training the BERT model for the adversarial removal
+for model_type in "bert-base-uncased"
+do
+    for loss_type in "x_entropy"
+    do
+        for hretrain in "no_warm_encoder"
+        do
+            for run in 0
+            do
+                for noise in 0.0
+                do
+                    for sample in 10000
+                    do
+                        for l2_lambd in 0.0
+                        do
+                            for dropout_rate in 0.0
+                            do
+                                for mainepoch in 0
+                                do
+                                    for mainmode in "non_causal"
+                                    do
+                                        for hlayer in 0
+                                        do
+                                            for advepoch in 5
+                                            do
+                                                for grstrength in 0.0
+                                                do
+                                                    for remmode in "adversarial"
+                                                    do
+                                                        for adv_rm_method in "adv_rm_only_topic"
+                                                        do
+                                                            for p in 0.5 0.6 0.7 0.8 0.9 0.99
+                                                            do
+                                                                python transformer_debugger.py -expt_num "pt.rel.model_type($model_type).remmode($remmode).adv_rm_method($adv_rm_method).grstrength($grstrength).advepoch($advepoch).lt($loss_type).dropout_rate($dropout_rate).l2($l2_lambd).hretrain($hretrain).noise($noise).hlayer($hlayer).sample($sample).p($p).run($run)" -num_sample $sample -num_topics 1 -num_epochs $mainepoch -path "dataset/multinli_1.0/" -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer -stage 2 -main_model_mode $mainmode --normalize_emb -lr 5e-5 -head_retrain_mode $hretrain -l2_lambd $l2_lambd -loss_type $loss_type -adv_rm_epochs $advepoch -rev_grad_strength $grstrength -debug_tidx 0 -removal_mode $remmode -dtype "nlp" -dropout_rate $dropout_rate -dtype "nlp_bert" --bert_as_encoder --train_bert -neg_topic_corr $p -transformer $model_type -adv_rm_method $adv_rm_method
+                                                            done
+                                                        done
+                                                    done
+                                                done       
+                                            done
+                                        done
+                                    done
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
 
 
 
