5f7e94ea24df762fdb0385efe454d5d78995a31c
BigBang0072 <abhinavkumar.sieger@gmail.com>
2022-04-23 12:42:22 +0530

    Added the experiments for large samples and l2 reg on bert model

--- multi_nli_downloader.sh
+++ multi_nli_downloader.sh
@@ -2,4 +2,4 @@
 #https://cims.nyu.edu/~sbowman/multinli/
 
 wget -O "./dataset/multinli_1.0.zip" "https://cims.nyu.edu/~sbowman/multinli/multinli_1.0.zip"
-unzip ./datset/multinli_1.0.zip
+unzip ./dataset/multinli_1.0.zip -d ./dataset/
