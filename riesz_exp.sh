37510c06bd37c3c89bea6173c27e924200980419
Abhinav Kumar <t-abkumar@microsoft.com>
2022-11-14 23:31:10 +0530

    Minor fix

--- riesz_exp.sh
+++ riesz_exp.sh
@@ -7,6 +7,7 @@ mkdir logs
 run_num=1
 mainepoch=8
 hlayer=0
+num_postalpha_layer=0
 main_mode="non_causal"
 cbsize=20
 stage_mode="stage1_riesz"
@@ -27,7 +28,7 @@ do
                     do
                         for pvalt1 in 0.9
                         do
-                            python transformer_debugger.py -expt_num "cad.s1riesz.rnum($run_num).pvalt0($pvalt0).pvalt1($pvalt1).dtidx($debug_tidx).rr_lmd($rr_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path "dataset/nlp_toy2/"  -out_path "dataset/nlp_toy2" -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype "toynlp2" -loss_type "x_entropy"  -topic0_corr $pvalt0 -topic1_corr $pvalt1 -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda
+                            python transformer_debugger.py -expt_num "cad.s1riesz.rnum($run_num).pvalt0($pvalt0).pvalt1($pvalt1).dtidx($debug_tidx).rr_lmd($rr_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path "dataset/nlp_toy2/"  -out_path "dataset/nlp_toy2" -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype "toynlp2" -loss_type "x_entropy"  -topic0_corr $pvalt0 -topic1_corr $pvalt1 -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -num_postalpha_layer $num_postalpha_layer
                         done
                     done
                 done
