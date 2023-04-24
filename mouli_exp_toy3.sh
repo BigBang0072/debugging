f7bd86fc5182621921b0b576ab461af7de4c8d0f
Abhinav Kumar <t-abkumar@microsoft.com>
2023-04-24 05:29:32 +0000

    Saving the mouli's resutls and previous hcanges and analysis

--- mouli_exp_toy3.sh
+++ mouli_exp_toy3.sh
@@ -10,7 +10,6 @@ max_len=20
 # batch_size=32
 batch_size=32
 lr=5e-3
-hlayer=0
 
 
 
@@ -24,7 +23,7 @@ path="dataset/nlp_toy3/"
 out_path="dataset/nlp_toy3"
 dtype="toynlp3"
 num_topics=1
-topic_name="spurious" #causal or spurious --> based on them the dataset will be created
+# topic_name="" #causal or spurious --> based on them the dataset will be created
 sample=1000
 dcf=0.0
 pos_size=1
@@ -33,18 +32,23 @@ pos_size=1
 
 for run_num in 0 1 2 #14 15 #10 11 12 #1 2
 do
-    for noise in 0.0
-    do
-        for dcf in 0.0 0.5 1.0
+    for topic_name in "causal" "spurious"
+    do 
+        for noise in 0.3
         do
-            for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.95 0.99
-            do
-                for mvsel_mode in "loss" "acc"
-                do 
-                    python transformer_debugger.py -expt_num "cad.moulis1toy3.rnum($run_num).topic($topic_name).sample($sample).noise($noise).pvaltsp($pvaltsp).dcf($dcf).mvsel($mvsel_mode)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -cfactuals_bsize $cbsize -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -vocab_path "assets/word2vec_10000_200d_labels.tsv" -max_len $max_len  -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr $lr -batch_size $batch_size -dtype $dtype -loss_type "x_entropy" -cfactuals_bsize $cbsize  -num_pos_sample $pos_size -num_neg_sample $pos_size  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx -stage_mode $stage_mode -sp_topic_pval $pvaltsp -degree_confoundedness $dcf  -topic_name $topic_name -mouli_valid_sel_mode $mvsel_mode &    #--bert_as_encoder -transformer $transformer --train_bert
+            for hlayer in 1 
+            do 
+                for dcf in 0.0 #0.5 1.0
+                do
+                    for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.95 0.99
+                    do
+                        for mvsel_mode in "loss" "acc"
+                        do 
+                            python transformer_debugger.py -expt_num "cad.moulis1toy3.rnum($run_num).topic($topic_name).sample($sample).noise($noise).pvaltsp($pvaltsp).dcf($dcf).mvsel($mvsel_mode)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -cfactuals_bsize $cbsize -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -vocab_path "assets/word2vec_10000_200d_labels.tsv" -max_len $max_len  -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr $lr -batch_size $batch_size -dtype $dtype -loss_type "x_entropy" -cfactuals_bsize $cbsize  -num_pos_sample $pos_size -num_neg_sample $pos_size  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx -stage_mode $stage_mode -sp_topic_pval $pvaltsp -degree_confoundedness $dcf  -topic_name $topic_name -mouli_valid_sel_mode $mvsel_mode &    #--bert_as_encoder -transformer $transformer --train_bert
+                        done
+                    done
                 done
             done
         done
-    done
+    done 
 done
-
--- riesz_exp_toy3.sh
+++ riesz_exp_toy3.sh
@@ -20,8 +20,7 @@ path="dataset/nlp_toy3/"
 out_path="dataset/nlp_toy3"
 dtype="toynlp3"
 num_topics=1
-topic_name="spurious" #causal or spurious --> based on them the dataset will be created
-
+topic_name="causal" #causal or spurious --> based on them the dataset will be created
 
 
 
@@ -40,15 +39,15 @@ topic_name="spurious" #causal or spurious --> based on them the dataset will be 
 #                     do
 #                         for tmle_lambda in 0
 #                         do
-#                             for l2_lambda in 0.0 # 0.1 1.0 10.0 100.0 200.0 1000.0
+#                             for l2_lambda in 0.0  0.1 1.0 10.0 100.0 200.0 1000.0
 #                             do
 #                                 for noise in 0.0
 #                                 do 
-#                                     for dcf in 0.0
+#                                     for dcf in 1.0
 #                                     do 
 #                                         for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.99
 #                                         do
-#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode -best_gval_selection_metric "acc" --select_best_gval --select_best_alpha &  #--debug_mode #--concat_word_emb  #--round_gval
+#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise).topic($topic_name)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode -best_gval_selection_metric "acc" --select_best_gval --select_best_alpha &  #--debug_mode #--concat_word_emb  #--round_gval
 #                                         done
 #                                     done
 #                                 done
@@ -76,15 +75,15 @@ topic_name="spurious" #causal or spurious --> based on them the dataset will be 
 #                     do
 #                         for tmle_lambda in 0
 #                         do
-#                             for l2_lambda in 0.0 # 0.1 1.0 10.0 100.0 200.0 1000.0 #we dont need this when h=0
+#                             for l2_lambda in 0.0 0.1 1.0 10.0 100.0 200.0 1000.0 #we dont need this when h=0
 #                             do
 #                                 for noise in 0.0
 #                                 do 
-#                                     for dcf in 0.0
+#                                     for dcf in 1.0
 #                                     do 
 #                                         for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.99
 #                                         do
-#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode -best_gval_selection_metric "loss" --select_best_gval --select_best_alpha & #--concat_word_emb  #--round_gval
+#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise).topic($topic_name)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode -best_gval_selection_metric "loss" --select_best_gval --select_best_alpha & #--concat_word_emb  #--round_gval
 #                                         done
 #                                     done
 #                                 done
@@ -101,46 +100,7 @@ topic_name="spurious" #causal or spurious --> based on them the dataset will be 
 
 # The experiemnt is for direct effect witou any rr component and any selection
 # Last time we forgot to switch off the best gval selection 
-# reg_mode="mse"
-# for run_num in  9 10 11 #10 11 #9 10 11 #9 10 11
-# do 
-#     for hlayer in 1 #1 2
-#     do 
-#         for debug_tidx in 0
-#         do
-#             for sample in 1000
-#             do
-#                 for reg_lambda in 1 
-#                 do 
-#                     for rr_lambda in 0
-#                     do
-#                         for tmle_lambda in 0
-#                         do
-#                             for l2_lambda in  0.0 #0.0 0.1 1.0 10.0 100.0 200.0 1000.0 #we dont need this when h=0
-#                             do
-#                                 for noise in 0.0
-#                                 do 
-#                                     for dcf in 0.0
-#                                     do 
-#                                         for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.99
-#                                         do
-#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode & #&    # -best_gval_selection_metric "loss" & #--concat_word_emb  #--round_gval
-#                                         done
-#                                     done
-#                                 done
-#                             done
-#                         done
-#                     done
-#                 done
-#             done
-#         done
-#     done
-# done
-
-
-
-
-for run_num in  18 19 20 #0 1 2
+for run_num in  10 11 #10 11 #9 10 11 #9 10 11
 do 
     for hlayer in 1 #1 2
     do 
@@ -150,19 +110,19 @@ do
             do
                 for reg_lambda in 1 
                 do 
-                    for rr_lambda in 1
+                    for rr_lambda in 0
                     do
                         for tmle_lambda in 0
                         do
-                            for l2_lambda in 0.0 #0.1 1.0 10.0 100.0 200.0 1000.0
+                            for l2_lambda in  0.0 1.0 10.0 100.0 200.0 1000.0 #we dont need this when h=0
                             do
                                 for noise in 0.0
                                 do 
-                                    for dcf in 0.0
+                                    for dcf in 1.0
                                     do 
                                         for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.99
                                         do
-                                            python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode & #--concat_word_emb  #--round_gval
+                                            python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise).topic($topic_name)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode & #&    # -best_gval_selection_metric "loss" & #--concat_word_emb  #--round_gval
                                         done
                                     done
                                 done
@@ -174,3 +134,41 @@ do
         done
     done
 done
+
+
+
+
+# for run_num in  18 19 20 #0 1 2
+# do 
+#     for hlayer in 1 #1 2
+#     do 
+#         for debug_tidx in 0
+#         do
+#             for sample in 1000
+#             do
+#                 for reg_lambda in 1 
+#                 do 
+#                     for rr_lambda in 1
+#                     do
+#                         for tmle_lambda in 0
+#                         do
+#                             for l2_lambda in 0.0 0.1 1.0 10.0 100.0 200.0 1000.0
+#                             do
+#                                 for noise in 0.0
+#                                 do 
+#                                     for dcf in 1.0
+#                                     do 
+#                                         for pvaltsp in 0.5 0.6 0.7 0.8 0.9 0.99
+#                                         do
+#                                             python transformer_debugger.py -expt_num "cad.toy3s1riesz.rnum($run_num).sample($sample).hlayer($hlayer).pvaltsp($pvaltsp).dtidx($debug_tidx).rr_lmd($rr_lambda).reg_lmd($reg_lambda).tmle_lmd($tmle_lambda).l2_lmd($l2_lambda).dcf($dcf).noise($noise).topic($topic_name)" -num_sample $sample -num_topics 2 -num_epochs $mainepoch -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr 5e-3 -dtype $dtype  -loss_type "x_entropy" -sp_topic_pval $pvaltsp -cfactuals_bsize $cbsize  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx  -stage_mode $stage_mode -rr_lambda $rr_lambda -tmle_lambda $tmle_lambda -l2_lambd $l2_lambda -reg_lambda $reg_lambda -num_postalpha_layer $num_postalpha_layer -batch_size $batch_size -max_len 20 -degree_confoundedness $dcf -topic_name $topic_name -riesz_reg_mode $reg_mode & #--concat_word_emb  #--round_gval
+#                                         done
+#                                     done
+#                                 done
+#                             done
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
