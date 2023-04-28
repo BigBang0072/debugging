9ab06d879650fb0a45f37955bef04310deaa8413
Abhinav Kumar <t-abkumar@microsoft.com>
2023-04-28 12:47:46 +0000

    Saving the mouli expt results

--- mouli_exp_mnist.sh
+++ mouli_exp_mnist.sh
@@ -30,11 +30,11 @@ pos_size=1
 
 
 
-for run_num in 0 #1 2 #1 2 #1 2 #14 15 #10 11 12 #1 2
+for run_num in 2  #1 2 #1 2 #1 2 #14 15 #10 11 12 #1 2
 do
     for topic_name in "all" # for mouli expt we internally give topic name
     do 
-        for noise in 0.0
+        for noise in 0.3
         do
             for hlayer in 1 
             do 
@@ -44,7 +44,7 @@ do
                     do
                         for mvsel_mode in "loss" #"acc"
                         do 
-                            python transformer_debugger.py -expt_num "cad.moulis1mnist.rnum($run_num).topic($topic_name).sample($sample).noise($noise).pvaltsp($pvaltsp).dcf($dcf).mvsel($mvsel_mode)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -cfactuals_bsize $cbsize -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -vocab_path "assets/word2vec_10000_200d_labels.tsv" -max_len $max_len  -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr $lr -batch_size $batch_size -dtype $dtype -loss_type "x_entropy" -cfactuals_bsize $cbsize  -num_pos_sample $pos_size -num_neg_sample $pos_size  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx -stage_mode $stage_mode -sp_topic_pval $pvaltsp -degree_confoundedness $dcf  -topic_name $topic_name -mouli_valid_sel_mode $mvsel_mode  #--bert_as_encoder -transformer $transformer --train_bert
+                            python transformer_debugger.py -expt_num "cad.moulis1mnist.rnum($run_num).topic($topic_name).sample($sample).noise($noise).pvaltsp($pvaltsp).dcf($dcf).mvsel($mvsel_mode)" -num_sample $sample -num_topics $num_topics -num_epochs $mainepoch -cfactuals_bsize $cbsize -path $path  -out_path $out_path -emb_path "glove-wiki-gigaword-100" -vocab_path "assets/word2vec_10000_200d_labels.tsv" -max_len $max_len  -noise_ratio $noise -num_hidden_layer $hlayer  -main_model_mode $main_mode --normalize_emb -lr $lr -batch_size $batch_size -dtype $dtype -loss_type "x_entropy" -cfactuals_bsize $cbsize  -num_pos_sample $pos_size -num_neg_sample $pos_size  -run_num $run_num -dropout_rate 0.0 -debug_tidx $debug_tidx -stage_mode $stage_mode -sp_topic_pval $pvaltsp -degree_confoundedness $dcf  -topic_name $topic_name -mouli_valid_sel_mode $mvsel_mode & #--bert_as_encoder -transformer $transformer --train_bert
                         done
                     done
                 done
